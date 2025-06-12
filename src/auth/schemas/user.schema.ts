f1bced97166a56afb6ce0163fbb34a4b8f335d14
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2025-06-12 13:28:24 +0300

    Add user Scheam

---
+++ src/auth/schemas/user.schema.ts
@@ -0,0 +1,14 @@
+import { Prop, SchemaFactory } from '@nestjs/mongoose';
+
+export class User extends Document {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
