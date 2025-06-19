55e0c92bef5e18832ead7856333029597d5e7f1d
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2025-06-19 13:36:21 +0300

    feat(user): implement getUserDocument method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -9,6 +9,7 @@ import { FilterQuery } from 'mongoose';
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
   async create(data: RegisterDto): Promise<User> {
     return await new this.userModel({
       ...data,
@@ -18,7 +19,10 @@ export class UserService {
 
   async isUserExist(query: FilterQuery<User>): Promise<boolean> {
     const userExists = await this.userModel.exists(query);
-
     return !!userExists; // Return true if user exists, otherwise false
+  }
+
+  async getUserDocument(query: FilterQuery<User>) {
+    return await this.userModel.findOne(query);
   }
 }
