33193fd48268e76b703acd663677d9b51e7c1ea7
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2025-06-22 15:15:05 +0300

    chore: mark GoogleStrategy as provider in AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,10 +6,17 @@ import { LocalStrategy } from './strategies/local.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { RefreshStrategy } from './strategies/refresh.strategy';
+import { GoogleStrategy } from './strategies/google.strategy';
 
 @Module({
   imports: [UserModule, PassportModule],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy, JwtStrategy, RefreshStrategy],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    JwtStrategy,
+    RefreshStrategy,
+    GoogleStrategy,
+  ],
 })
 export class AuthModule {}
