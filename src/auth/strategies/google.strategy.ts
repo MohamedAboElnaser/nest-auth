13935efe7e0f2dc756a3b3fd579c73158df52ddb
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2025-06-22 15:14:01 +0300

    feat(auth): implement GoogleAuthGuard and GoogleStrategy

---
+++ src/auth/strategies/google.strategy.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { AuthService } from '../auth.service';
+import * as fs from 'fs';
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor(
+    private config: ConfigService,
+    private authService: AuthService,
+  ) {
+    super({
+      clientID: config.get('GOOGLE_CLIENT_ID'),
+      clientSecret: config.get<string>('GOOGLE_CLIENT_SECRET'),
+      callbackURL: config.get<string>('GOOGLE_CALLBACK_URL'),
+      scope: ['email', 'profile'],
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: VerifyCallback,
+  ) {
+    // fs.writeFile('profile.json', JSON.stringify(profile), () => {});
+    const user = await this.authService.validateGoogleUser({
+      email: profile.email[0].value,
+      name: profile.displayName,
+    });
+    done(null, user);
+  }
+}
