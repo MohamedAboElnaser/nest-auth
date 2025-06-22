425696366729678e51275917af9fd9e82412c6db
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2025-06-22 15:24:54 +0300

    feat(auth): Implement Google OAuth workflow

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,19 @@
-import { Body, Controller, Post, UseGuards, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  Req,
+  Res,
+  Get,
+} from '@nestjs/common';
 import { RegisterDto } from './dtos/register.dto';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dtos/login.dto';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { Response } from 'express';
 import { RefreshJwtAuthGuard } from './guards/refresh.guard';
+import { GoogleAuthGuard } from './guards/google-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -63,5 +72,38 @@ export class AuthController {
     return {
       access_token,
     };
+  }
+
+  @Get('google')
+  @UseGuards(GoogleAuthGuard)
+  googleAuth() {
+    /**
+     *  This handler will never be executed
+     *  because GoogleStrategy will redirect user to Google Concent screen
+     */
+  }
+
+  @Get('google/callback')
+  @UseGuards(GoogleAuthGuard)
+  async googleAuthRedirect(@Req() req, @Res({ passthrough: true }) res) {
+    const { access_token, refresh_token } = this.authService.generateTokens(
+      res.user.email,
+      res.user._id,
+    );
+
+    res.cookie('refresh_token', refresh_token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge:
+        parseInt(process.env.REFRESH_TOKEN_COOKIE_EXPIRES_IN_DAYS) *
+        24 *
+        60 *
+        60 *
+        1000,
+    });
+
+    // Redirect to frontend with access token
+    return res.redirect(`${process.env.FRONTEND_URL}?token=${access_token}`);
   }
 }
